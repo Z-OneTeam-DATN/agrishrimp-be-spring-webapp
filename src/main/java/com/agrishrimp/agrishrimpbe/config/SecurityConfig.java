d900ae7f437fac1b844e121d6ab9a9dde0f9965e
Bao Nhien <bhnhien40@gmail.com>
2025-12-25 14:35:54 +0700

    chore: setup swagger ui and openapi configuration

--- src/main/java/com/agrishrimp/agrishrimpbe/config/SecurityConfig.java
+++ src/main/java/com/agrishrimp/agrishrimpbe/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
 
                         // Cho phép xem tài liệu API
-                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
 
                         // b.PRIVATE
                         .anyRequest().authenticated()
