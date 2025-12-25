5ead800d7452aab5aa5578075f566ba721886d01
Bao Nhien <bhnhien40@gmail.com>
2025-12-25 14:03:43 +0700

    chore(security): add SecurityException handling case

---
+++ src/main/java/com/agrishrimp/agrishrimpbe/exception/ConflictException.java
@@ -0,0 +1,20 @@
+package com.agrishrimp.agrishrimpbe.exception;
+import com.agrishrimp.agrishrimpbe.utils.MessagesUtils;
+
+import lombok.Setter;
+
+@Setter
+public class ConflictException extends RuntimeException {
+
+    private String message;
+
+    public ConflictException(String errorCode, Object... var2) {
+        this.message = MessagesUtils.getMessage(errorCode, var2);
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+}
